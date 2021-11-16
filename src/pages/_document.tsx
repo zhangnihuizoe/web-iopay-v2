b0e107c51af5d4254a968f780eb50d102a2add7b
anshuomomo@gmail.com <anshuomomo@gmail.com>
2021-11-16 17:07:37 +0800

    Update font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default class Document extends NextDocument {
       <Html>
           <title>IoPay - Official IOTEX crypto wallet for IOTX Token. Get It Here</title>
           <meta name="description" content="IoPay provdies most secure wallet for IOTX users. IOTEX is blockchain that powers ecosystemy of smart devices. Get wallet here...." />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                rel="stylesheet" />
+          <link href="https://fonts.font.im/css?family=Noto+Sans" rel="stylesheet" />
         <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
